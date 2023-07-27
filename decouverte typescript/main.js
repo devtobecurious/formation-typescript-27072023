5df458d15a87899b14b6478bfd131b6722bb8609
Evan BOISSONNOT <evan@boissonnot.fr>
2023-07-27 14:04:40 +0200

    feat: premiere class ts

---
+++ decouverte typescript/main.js
@@ -0,0 +1,11 @@
+"use strict";
+var Yolo = /** @class */ (function () {
+    function Yolo() {
+    }
+    Yolo.prototype.osef = function () {
+        console.log("osef");
+    };
+    return Yolo;
+}());
+var y = new Yolo();
+y.osef();
---
+++ decouverte typescript/main.ts
@@ -0,0 +1,7 @@
+class Yolo {
+    osef() {
+        console.log("osef");
+    }
+}
+var y = new Yolo();
+y.osef();
